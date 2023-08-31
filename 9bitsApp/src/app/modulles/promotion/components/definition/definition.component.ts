73f7c9ce8b4cb4bb54179312120349c11557c55e
michalc10 <michalc1101@gmail.com>
2023-08-31 11:21:12 +0200

    saving to localstorange/ return data after refresh

--- 9bitsApp/src/app/modulles/promotion/components/definition/definition.component.ts
+++ 9bitsApp/src/app/modulles/promotion/components/definition/definition.component.ts
@@ -1,18 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { MatDatepicker, MatDatepickerInputEvent } from '@angular/material/datepicker';
+import { LocalStorageService } from 'src/app/modulles/shared/services/local-storage.service';
+
 
 @Component({
   selector: 'app-definition',
   templateUrl: './definition.component.html',
   styleUrls: ['./definition.component.scss']
 })
-export class DefinitionComponent {
+export class DefinitionComponent implements OnInit {
 
   foods: any[] = [
-    {value: 'steak-0', viewValue: 'Steak'},
-    {value: 'pizza-1', viewValue: 'Pizza'},
-    {value: 'tacos-2', viewValue: 'Tacos'},
+    { value: 'steak-0', viewValue: 'Steak' },
+    { value: 'pizza-1', viewValue: 'Pizza' },
+    { value: 'tacos-2', viewValue: 'Tacos' },
   ];
-  labelPosition=''
-  checked=false
-  indeterminate=false
+  labelPosition = ''
+  checked = false
+  indeterminate = false
+  definitionFormGroup: FormGroup;
+
+  // startDatePicker: MatDatepicker<any>;
+  // finishDatePicker: MatDatepicker<any>;
+
+
+  constructor(private localStorageService: LocalStorageService) { 
+    this. definitionFormGroup = new FormGroup({
+      marketingName: new FormControl('', [Validators.required]),
+      technicalName: new FormControl(''),
+      description: new FormControl(''),
+      conditionsUser: new FormControl('', [Validators.required]),
+      conditionType: new FormControl('', [Validators.required]),
+      startDate: new FormControl('', [Validators.required]),
+      finishDate: new FormControl(''),
+      paymentBenefits: new FormControl(''),
+      coectedWithOtherPromotions: new FormControl(''),
+      backPromotion: new FormControl(''),
+    })
+  }
+
+  ngOnInit() { 
+    Object.keys(this.definitionFormGroup.controls).forEach(controlName => {
+      const savedValue = localStorage.getItem(controlName);
+      if (savedValue) {
+        this.definitionFormGroup.controls[controlName].setValue(savedValue);
+      }
+    });
+  }
+
+  
+  saveTextToLocalStorage(key:string, value:string){
+    this.localStorageService.saveData(key, value)
+
+  }
+
+
 }
--- 9bitsApp/src/app/modulles/promotion/promotion.module.ts
+++ 9bitsApp/src/app/modulles/promotion/promotion.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { PromotionRoutingModule } from './promotion-routing.module';
 import { DefinitionComponent } from './components/definition/definition.component';
 import { SummaryComponent } from './components/summary/summary.component';
+import { ReactiveFormsModule } from '@angular/forms'
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatDatepickerModule } from '@angular/material/datepicker';
@@ -13,6 +14,9 @@ import { FormsModule } from '@angular/forms';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatIconModule } from '@angular/material/icon'
+import { MAT_DATE_LOCALE } from '@angular/material/core';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
 
 @NgModule({
   declarations: [
@@ -22,6 +26,7 @@ import { MatIconModule } from '@angular/material/icon'
   imports: [
     CommonModule,
     PromotionRoutingModule,
+    ReactiveFormsModule,
     MatInputModule,
     MatSelectModule,
     MatDatepickerModule,
@@ -31,7 +36,11 @@ import { MatIconModule } from '@angular/material/icon'
     FormsModule,
     MatCheckboxModule,
     MatBadgeModule,
-    MatIconModule
+    MatIconModule,
+    MatTooltipModule
+  ], providers: [
+    // Dodaj dostawcę MAT_DATE_LOCALE tutaj
+    { provide: MAT_DATE_LOCALE, useValue: 'en-GB' }
   ]
 })
 export class PromotionModule { }
